5f34d659320515ec5c0e91de5eb3f8a9de4188f4
Alex Liu <yuningalexliu@gmail.com>
2015-03-21 12:35:49 -0400

    able to add days

--- public/js/jquery.js
+++ public/js/jquery.js
@@ -1,11 +1,29 @@
+//Hotel code
+
 $( "button.add-hotels" ).on( "click", function() {
-  console.log($( "#selector-hotels option:selected" ).text());
+	var hotelName = $( "#selector-hotels option:selected" ).text();
+	console.log(hotelName);
+	$("#itinerary-hotels").append(
+		'<div class="itinerary-item" id="itinerary-hotels"><span class="title">' + hotelName + '</span><span class="remove"><button class="btn btn-danger btn-xs btn-circle pull-right">x</button></span></div>'
+	);
 });
+
+//Restaurant code
 
 $( "button.add-restaurants" ).on( "click", function() {
-  console.log($( "#selector-restaurants option:selected" ).text());
+	var restaurantName = $( "#selector-restaurants option:selected" ).text();
+	console.log(restaurantName);
+	$("#itinerary-restaurants").append(
+		'<div class="itinerary-item" id="itinerary-restaurants"><span class="title">' + restaurantName + '</span><span class="remove"><button class="btn btn-danger btn-xs btn-circle pull-right">x</button></span></div>'
+	);
 });
 
+//Things to do code
+
 $( "button.add-things_to_do" ).on( "click", function() {
-  console.log($( "#selector-things_to_do option:selected" ).text());
+	var thingToDoName = $( "#selector-things_to_do option:selected" ).text();
+	console.log(thingToDoName);
+	$("#itinerary-things_to_do").append(
+		'<div class="itinerary-item" id="itinerary-things_to_do"><span class="title">' + thingToDoName + '</span><span class="remove"><button class="btn btn-danger btn-xs btn-circle pull-right">x</button></span></div>'
+	);
 });
